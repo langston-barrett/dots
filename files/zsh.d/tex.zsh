24caf8fbf2bc48d458d26de03205106b188eeaff
Langston Barrett <langston.barrett@gmail.com>
2016-10-13 18:54:39 +0000

    cleantex: minor improvement

--- files/zsh.d/tex.zsh
+++ files/zsh.d/tex.zsh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
 
 # Description: delete all tex compilation extras in the current directory
-function cleantex() { remove_exts "toc log fls fdb bib fbd_latexmk out aux"; }
+function cleantex() { remove_exts "toc log fls fdb bib fdb_latexmk out aux synctex.gz"; }
