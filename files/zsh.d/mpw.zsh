ccd8f012c903a09929241086f8bffe13ba3f24ca
Langston Barrett <langston.barrett@gmail.com>
2018-06-26 10:20:54 -0700

    small changes

--- files/zsh.d/mpw.zsh
+++ files/zsh.d/mpw.zsh
@@ -1,7 +1,6 @@
 #!/usr/bin/env zsh
-export MPW_FULLNAME='Langston Barrett'
-export MPW_SITETYPE=x
-export MPW_ASKPASS=ksshaskpass
+export MP_FULLNAME='Langston Barrett'
+export MP_SITETYPE=x
 
 # https://github.com/Lyndir/MasterPassword/blob/master/platform-independent/cli-c/mpw.bashrc
 
