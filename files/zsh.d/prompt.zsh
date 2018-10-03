e29d6fecf830da42568b8bab6ac047f8e609a39c
Langston Barrett <langston.barrett@gmail.com>
2018-10-03 12:54:56 -0700

    tmux: directory in status bar

--- files/zsh.d/prompt.zsh
+++ files/zsh.d/prompt.zsh
@@ -12,9 +12,9 @@ function in_nix_shell() {
 
 # The convention is a single space at the end
 local dir_2='%{$fg_bold[0]%}%2~% %{$reset_color%}'
-local final_char='> %{$reset_color%}'
+local final_char=' %{$reset_color%}'
 local time='(%D{%K:%M}) %{$reset_color%}'
 local ns='$(in_nix_shell)'
 
-PROMPT="${dir_2} ${final_char}"
-RPROMPT="${ns}"
+PROMPT="${final_char}"
+RPROMPT=""
--- files/zsh.d/tmux.zsh
+++ files/zsh.d/tmux.zsh
@@ -2,7 +2,7 @@
 
 #### tmux dracula theme
 
-# ## Colors
+## Colors
 local black='colour16'
 local white='colour255'
 local gray='colour236'
@@ -11,7 +11,7 @@ local yellow='colour215'
 local light_purple='colour141'
 local dark_purple='colour61'
 
-# ## Icons
+## Icons
 local left_sep=''
 local right_sep=''
 local right_alt_sep=''
@@ -26,14 +26,31 @@ tmux set-option message-bg "${gray}"
 tmux set-option message-fg "${white}"
 tmux set-option message-command-bg "${gray}"
 tmux set-option message-command-fg "${white}"
-tmux set-option status-left " #I #[fg=${dark_gray},reverse]${right_sep} "
 tmux set-option status-left-style "fg=${white},bg=${dark_purple},bold"
-tmux set-option status-right "${left_sep}#[bg=${black},reverse] %Y-%m-%d %H:%M "
 tmux set-option status-right-style "fg=${light_purple},bg=${dark_gray}"
 tmux set-window-option window-status-activity-style "fg=${white},bg=${gray}"
 tmux set-window-option window-status-separator ''
 tmux set-window-option window-status-format ' #I #W '
 tmux set-window-option window-status-style "fg=${yellow},bg=${dark_gray}"
-tmux set-window-option window-status-current-format \
-  "${right_sep}#[fg=${black}] #I ${right_alt_sep} #W #[fg=${dark_gray},reverse]${right_sep}"
 tmux set-window-option window-status-current-style "fg=${dark_gray},bg=${light_purple}"
+
+# This sets it before every command so that we can get nix-shell info
+precmd () {
+  local black='colour16'
+  local white='colour255'
+  local gray='colour236'
+  local dark_gray='colour236'
+  local yellow='colour215'
+  local light_purple='colour141'
+  local dark_purple='colour61'
+  local left_sep=''
+  local right_sep=''
+  local right_alt_sep=''
+
+  tmux set-window-option window-status-current-format \
+       "${right_sep}#[fg=${black}] / ${right_alt_sep} $(dirname $PWD | cut -c 2-) ${right_alt_sep} $(basename $PWD) #[fg=${dark_gray},reverse]${right_sep}"
+  tmux set-option status-left "#[fg=${black}] #I ${right_alt_sep} #W #[fg=${dark_gray},reverse]${right_sep}"
+  tmux set-option status-right "${left_sep}#[bg=${black},reverse] $(in_nix_shell) | %Y-%m-%d %H:%M "
+  # tmux set-option status-right "${left_sep}#[bg=${black},reverse] %Y-%m-%d %H:%M "
+  # tmux set-option status-right-style "fg=${light_purple},bg=${dark_gray}"
+}
