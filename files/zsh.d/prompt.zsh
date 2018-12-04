7026e2c11ff08ea5f02c2fc11cb4ca0e04340a8e
Langston Barrett <langston.barrett@gmail.com>
2018-12-04 10:01:00 -0800

    zsh: prompt depends on being in tmux

--- files/zsh.d/prompt.zsh
+++ files/zsh.d/prompt.zsh
@@ -12,9 +12,18 @@ function in_nix_shell() {
 
 # The convention is a single space at the end
 local dir_2='%{$fg_bold[0]%}%2~% %{$reset_color%}'
-local final_char=' %{$reset_color%}'
+local final_char='> %{$reset_color%}'
 local time='(%D{%K:%M}) %{$reset_color%}'
 local ns='$(in_nix_shell)'
 
-PROMPT="${final_char}"
-RPROMPT=""
+# If we're in tmux, the directory is shown on the status line
+if [[ -z $TMUX ]]; then
+  PROMPT="${dir_2} ${final_char}"
+else
+  PROMPT="${final_char}"
+fi
+
+# If we're on another machine, show the user/hostname
+if [[ $(uname -n) != langston-nixos ]]; then
+  PROMPT="$(whoami)@$(uname -n) ${dir_2} > "
+fi
