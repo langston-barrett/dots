b5c5636d483cc4998a42380e5f5169cadd4beb14
Langston Barrett <langston.barrett@gmail.com>
2019-03-07 09:31:39 -0800

    zsh: github_clone

--- files/zsh.d/dev.zsh
+++ files/zsh.d/dev.zsh
@@ -49,6 +49,7 @@ alias gri='git rebase -i'
 alias grv='git remote -v'
 alias gs='git status'
 
+function github_clone { git clone "https://github.com/$1" }
 function git_clone_mine { git clone "https://github.com/siddharthist/$1" }
 
 ## Nix
@@ -88,3 +89,10 @@ proj_test () {
 
 GPG_TTY=$(tty)
 export GPG_TTY
+
+setopt HIST_IGNORE_DUPS     # Don't record an entry that was just recorded again.
+setopt HIST_IGNORE_ALL_DUPS # Delete old recorded entry if new entry is a duplicate.
+setopt HIST_FIND_NO_DUPS    # Do not display a line previously found.
+setopt HIST_IGNORE_SPACE    # Don't record an entry starting with a space.
+setopt HIST_SAVE_NO_DUPS    # Don't write duplicate entries in the history file.
+setopt HIST_REDUCE_BLANKS   # Remove superfluous blanks before recording entry.
