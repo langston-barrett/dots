eb3667a645bf6329c1db1bc16f69ffda60a4bb7a
Langston Barrett <langston.barrett@gmail.com>
2019-05-17 11:07:05 -0700

    new desk :)

--- files/monitor/at-desk.sh
+++ files/monitor/at-desk.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 xrandr --output HDMI2 --off --output HDMI1 --off \
-       --output DP1 --primary --mode 3840x2160 --pos 0x0 --rotate normal \
-       --output eDP1 --mode 1920x1080 --pos 3840x896 --rotate normal
+       --output DP1 --primary --mode 3840x2160 --pos 1920x0 --rotate normal \
+       --output eDP1 --mode 1920x1080 --pos 0x300 --rotate normal
 
 xrandr --dpi 190
 
