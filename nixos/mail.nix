ba9da83fbf569a9a90920b1bbf035a563681a3d4
Langston Barrett <langston.barrett@gmail.com>
2019-05-26 11:42:54 -0700

    ssh config: cfern

--- nixos/mail.nix
+++ nixos/mail.nix
@@ -2,6 +2,7 @@
 
 {
   environment.systemPackages = with pkgs; [
+    gnupg
     gpgme
     neomutt
     offlineimap
--- nixos/ssh.nix
+++ nixos/ssh.nix
@@ -7,23 +7,10 @@
     startAgent = true;
     agentTimeout = null; # forever
     extraConfig = ''
-      Host jump-eu
-          HostName 173.39.246.71
-          IdentityFile ~/.ssh/id_ed25519
-          Port 2222
-          User lbarrett
-
-      Host tx3
-          HostName 128.107.4.225
-          IdentityFile ~/.ssh/id_ed25519
-          Port 2222
-          User lbarrett
-
-      Host london
-          HostName 173.39.251.71
-          IdentityFile ~/.ssh/id_ed25519
-          Port 2222
-          User lbarrett
+      Host cfern
+        HostName ec2-54-188-130-129.us-west-2.compute.amazonaws.com
+        Port 22
+        User ubuntu
     '';
   };
 }
