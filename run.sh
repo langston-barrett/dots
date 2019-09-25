57120deacfe9c0f0e5f21fcb45492f7820e8dec3
Langston Barrett <langston.barrett@gmail.com>
2019-09-25 08:46:04 -0700

    run.sh: specify substituters

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 export XDG_CONFIG_HOME=$HOME/.config
-nix-shell default.nix --run \
+nix-shell default.nix  \
+          --substituters "https://cache.nixos.org" \
+          --run \
           'ansible-playbook --forks 100 \
                             -i inventory \
                             -e ansible_python_interpreter=$(which python) \
