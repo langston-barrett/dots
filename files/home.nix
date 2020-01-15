259c76445b0caaf9bfb4016a95182f0112d5351d
Langston Barrett <langston.barrett@gmail.com>
2020-01-15 10:54:13 -0800

    nixos, home-manager: migrate zsh configuration

--- files/home.nix
+++ files/home.nix
@@ -1,8 +1,7 @@
 { pkgs, ... }:
 
 {
-  home.packages = [
-  ];
+  home.packages = with pkgs; [fasd];
 
   services.dunst = {
     enable = true;
@@ -11,4 +10,44 @@
   # programs.home-manager = {
   #   enable = true;
   # };
+
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+    enableCompletion = true;
+    sessionVariables = {
+      # masterpassword
+      MPW_FULLNAME = "Langston Barrett";
+      MPW_SITETYPE = "x";
+      XDG_CONFIG_HOME = "$HOME/.config";
+      EDITOR = "emacs";
+    };
+    shellAliases = {
+      amiconnected = "while true; do if curl google.com &> /dev/null; then echo \"$(date +%H:%M) ~~~GOOD~~~\"; else echo \"$(date +%H:%M) ---BAD---\"; fi; sleep 5; done";
+      test-ssh = "ssh -T git@github.com";
+      ls1 = "ls -1";
+      screenshot = "import -window root ~/Downloads/screenshot.jpg";
+      # Pipe stuff to this command and get a URL back
+      pastebin = "curl -F \"clbin=<-\" https://clbin.com";
+
+      # Docker
+      docker-gc = "sudo docker ps -a -q -f status=exited | xargs --no-run-if-empty sudo docker rm";
+      docker-gc-images = "docker images -q | xargs --no-run-if-empty docker rmi || true";
+
+      # systemd
+      sys   = "sudo systemctl";
+      syss  = "sudo systemctl status";
+      sysr  = "sudo systemctl restart";
+      sysu  = "systemctl --user";
+      sysus = "systemctl --user status";
+      sysur = "systemctl --user restart";
+
+      top5  = "watch 'ps aux | sort -nrk 3,3 | head -n 5'";
+    };
+  };
+
+  programs.direnv = {
+    enable = true;
+    enableZshIntegration = true;
+  };
 }
--- nixos/zsh.nix
+++ nixos/zsh.nix
@@ -3,63 +3,6 @@
 {
   imports = [];
 
-  environment = {
-
-    systemPackages = with pkgs; [
-      direnv # automatically invoke/revoke a nix-shell
-      fasd
-      zsh-completions
-    ];
-
-    shellAliases = {
-      amiconnected = "while true; do if curl google.com &> /dev/null; then echo \"$(date +%H:%M) ~~~GOOD~~~\"; else echo \"$(date +%H:%M) ---BAD---\"; fi; sleep 5; done";
-      test-ssh = "ssh -T git@github.com";
-      ls1 = "ls -1";
-      reload = "source /etc/zshrc";
-      # TODO: imagemagick
-      screenshot = "import -window root ~/Downloads/screenshot.jpg";
-      conky = "conky --config=$XDG_CONFIG_HOME/conky/conkyrc";
-      nixpkgs-pr-review = "export TRAVIS_BUILD_DIR=$PWD && ./maintainers/scripts/travis-nox-review-pr.sh nixpkgs-verify nixpkgs-manual nixpkgs-tarball && ./maintainers/scripts/travis-nox-review-pr.sh nixos-options nixos-manual";
-
-      # Pipe stuff to this command and get a URL back
-      pastebin = "curl -F \"clbin=<-\" https://clbin.com";
-
-      # Docker
-      docker-gc = "sudo docker ps -a -q -f status=exited | xargs --no-run-if-empty sudo docker rm";
-      docker-gc-images = "docker images -q | xargs --no-run-if-empty docker rmi || true";
-
-      # systemd
-      sys   = "sudo systemctl";
-      syss  = "sudo systemctl status";
-      sysr  = "sudo systemctl restart";
-      sysu  = "systemctl --user";
-      sysus = "systemctl --user status";
-      sysur = "systemctl --user restart";
-
-      top5  = "watch 'ps aux | sort -nrk 3,3 | head -n 5'";
-
-      # Convenience
-      # TODO: nix function
-      work-algebra = ''
-        cd ~/Dropbox/langston/tex/math332; \
-          zathura dummit.djvu & disown; \
-          zathura notes/tex/1-ring-theory.pdf & disown; \
-          ${pkgs.emacs}/bin/emacsclient --create-frame & disown; \
-          sleep 1; exit 0
-      '';
-    };
-
-    variables = {
-      XDG_CONFIG_HOME = "$HOME/.config";
-      EDITOR           = "emacs";
-      #PATH            = "$PATH:$XDG_CONFIG_HOME/bin";
-
-      # masterpassword
-      MPW_FULLNAME = "Langston Barrett";
-      MPW_SITETYPE = "x";
-    };
-  };
-
   programs.zsh = {
     enable = true;
     enableCompletion = true;
@@ -84,13 +27,6 @@
       fi
       source_all() {[[ -d $1 ]] && for f in $1/*.zsh; do source "$f"; done; unset f;}
       source_all $HOME/.zsh.d
-
-      # http://www.zsh.org/mla/users/2007/msg00944.html
-      # https://goo.gl/CsT6cQ
-      # TMOUT=30
-      # TRAPALRM () {
-      #   zle reset-prompt
-      # }
     '';
   };
 
