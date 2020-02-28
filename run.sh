29f9f88464a051bcdd6f7bf511f8dede34dba8f1
Langston Barrett <langston.barrett@gmail.com>
2020-02-28 09:42:43 -0800

    run.sh: more automation

--- run.sh
+++ run.sh
@@ -10,20 +10,36 @@ bail_if_not_installed() {
   fi
 }
 
-if ! installed nix; then
-  bail_if_not_installed curl
-  curl https://nixos.org/nix/install | sh
-fi
+bail_if_not_installed curl
+bail_if_not_installed tar
 
-if [[ -n $XDG_CONFIG_HOME ]]; then
-  export XDG_CONFIG_HOME=$HOME/.config
-fi
+if ! [[ -f scripts/run-ansible.sh ]]; then
+  dir=$(mktemp -d)
+  pushd "$dir"
+  curl \
+    --location \
+    --output "$PWD/dots.tar.gz" \
+    "https://github.com/langston-barrett/dots/tarball/master"
+  tar xvf dots.tar.gz
+  pushd ./langston-barrett-dots*/
+  bash run.sh
+  popd
+  popd
 
-# nix-shell default.nix  \
-#           --substituters "https://cache.nixos.org" \
-#           --run \
-#           'bash scripts/shellcheck.sh'
-nix-shell default.nix  \
-          --substituters "https://cache.nixos.org" \
-          --run \
-          "bash scripts/run-ansible.sh"
+else
+  if ! installed nix; then
+    curl https://nixos.org/nix/install | sh
+  fi
+
+  if [[ -n $XDG_CONFIG_HOME ]]; then
+    export XDG_CONFIG_HOME=$HOME/.config
+  fi
+  # nix-shell default.nix  \
+  #           --substituters "https://cache.nixos.org" \
+  #           --run \
+  #           'bash scripts/shellcheck.sh'
+  nix-shell default.nix  \
+            --substituters "https://cache.nixos.org" \
+            --run \
+            "bash scripts/run-ansible.sh"
+fi
