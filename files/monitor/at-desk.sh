3f0387717dd39750648585713d1b8dddb42c954f
Langston Barrett <langston.barrett@gmail.com>
2020-08-07 16:09:21 -0700

    spacemacs: helm-dash

--- files/monitor/at-desk.sh
+++ files/monitor/at-desk.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 xrandr --output HDMI2 --off --output HDMI1 --off \
-       --output DP1 --primary --mode 3840x2160 --pos 1920x0 --rotate normal \
-       --output eDP1 --mode 1920x1080 --pos 0x300 --rotate normal
+       --output DP1 --primary --mode 2560x1440 --pos 0x0 --rotate normal \
+       --output eDP1 --mode 1920x1080 --pos 2560x300 --rotate normal
 
 xrandr --dpi 190
 
