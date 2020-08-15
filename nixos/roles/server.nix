bea8a1e9ab03c27e439ebaf4e92e129fd445fd22
Langston Barrett <langston.barrett@gmail.com>
2020-08-15 07:12:46 -0700

    monitoring

--- nixos/roles/server.nix
+++ nixos/roles/server.nix
@@ -30,27 +30,4 @@
       defaultWindowManager = "i3";  # default: xterm
     };
   };
-
-  systemd.user.services = {
-    tigervnc = {
-      # TODO: Find which setting makes it so tigervnc can't drop capabilities
-      enable = false;
-      description = "tigervnc";
-      serviceConfig =
-        import ../functions/service.nix { userService = true; } // {
-          ExecStart = "${pkgs.tigervnc}/bin/vncserver";
-          Restart = "always";
-          RestartSec = "5s";
-          MemoryLimit = "4G";
-          MemorySwapMax = "0M";
-          PrivateNetwork = false;
-          NoNewPrivileges = false; # TODO maybe remove?
-        };
-    };
-  };
-
-  networking.firewall.allowedTCPPorts = [
-    5500 # tigervnc
-    5901 # tigervnc
-  ];
 }
