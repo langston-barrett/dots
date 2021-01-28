dd32f314a2cdd409b4531cbdb89aa574c133feb6
Langston Barrett <langston.barrett@gmail.com>
2021-01-28 14:57:45 -0800

    crucible shell

---
+++ files/nix/crucible/shell.nix
@@ -0,0 +1,16 @@
+with import <nixpkgs> { };
+
+mkShell {
+  shellHook = ''
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${zlib}/lib
+  '';
+  buildInputs =  [
+    yices
+    z3
+    (haskell.packages.ghc884.ghcWithHoogle (hpkgs: with hpkgs; []))
+    haskellPackages.haskell-language-server
+    haskellPackages.ghcid
+    haskellPackages.cabal-install
+    zlib
+  ];
+}
