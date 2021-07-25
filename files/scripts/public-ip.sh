bf8eab31ff1046c74e009028d91e16f0b1c5bfeb
Langston Barrett <langston.barrett@gmail.com>
2021-07-25 13:02:00 -0700

    more

--- files/scripts/public-ip.sh
+++ files/scripts/public-ip.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -eu
+
 ip=$(dig TXT +short o-o.myaddr.l.google.com @ns1.google.com | tr -d '"')
 echo ${ip} > /tmp/ip.txt
-echo "${ip}" | gh gist create -d ip-$(date --iso-8601).txt
+echo "${ip}" | gh gist create --filename "ip-$(date --iso-8601).txt"
