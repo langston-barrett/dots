35a6d7edcb4013a6a2c00b47a42fa7cda903aad4
Langston Barrett <langston.barrett@gmail.com>
2021-07-26 11:17:29 -0700

    more

---
+++ files/nix/polymorph/shell.nix
@@ -0,0 +1,27 @@
+{ ghc ? "ghc8104"
+, unstableHaskell ? false
+}:
+
+let
+  pkgs = import <nixpkgs> { };
+  unstable = import <unstable> { };
+  hPkgs = if unstableHaskell then unstable else pkgs;
+in pkgs.mkShell {
+  shellHook = ''
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.zlib}/lib
+  '';
+  buildInputs =  [
+    pkgs.clang_8
+    pkgs.llvm_8
+    pkgs.yices
+    pkgs.z3
+
+    (hPkgs.haskell.packages.${ghc}.ghcWithHoogle (hpkgs: with hpkgs; []))
+    # hPkgs.haskell-language-server
+    hPkgs.haskellPackages.ormolu
+    hPkgs.haskellPackages.ghcid
+    hPkgs.haskellPackages.hlint
+    hPkgs.haskellPackages.cabal-install
+    pkgs.zlib  # needed for Haskell
+  ];
+}
