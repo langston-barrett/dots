cb078e18aec579265707fcfa4e58917be4f13f80
Langston Barrett <langston.barrett@gmail.com>
2021-12-05 09:07:38 -0500

    .

--- files/monitor/at-desk.sh
+++ files/monitor/at-desk.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 xrandr \
-  --output DP1 --primary --mode 3840x2160 --pos 0x0 --rotate normal \
-  --output eDP1 --off \
-  --output DP2 --off \
-  --output HDMI1 --off \
-  --output HDMI2 --off \
-  --output VIRTUAL1 --off
+  --output DP-1 --primary --mode 3840x2160 --pos 0x0 --rotate normal \
+  --output eDP-1 --off \
+  --output DP-2 --off \
+  --output HDMI-1 --off \
+  --output HDMI-2 --off \
+  --output VIRTUAL-1 --off
 
 # xrandr --dpi 190
 
