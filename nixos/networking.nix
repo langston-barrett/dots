2c2867a932c0ba695f4266922112c082aaf75011
Langston Barrett <langston.barrett@gmail.com>
2022-12-19 09:35:42 -0500

    .

--- nixos/networking.nix
+++ nixos/networking.nix
@@ -7,60 +7,6 @@
   networking = {
     wg-quick = {
       interfaces = {
-        pdx =
-          let awk = "${pkgs.gawk}/bin/awk";
-              endpoint = "65.132.32.180";
-          in {
-          privateKeyFile = "/root/vpn/wg_pdx.private.key";
-          address = [
-            "10.20.10.87"
-            "2001:428:6002:410::87"
-          ];
-          dns = ["10.20.10.1" "galois.com"];
-          postUp = ''
-            ip route add ${endpoint} via $(ip -o -4 route show to default | ${awk} '{print $3}') dev $(ip -o -4 route show to default | ${awk} '{print $5}')
-          '';
-          postDown = ''
-            ip route del ${endpoint} via $(ip -o -4 route show to default | ${awk} '{print $3}') dev $(ip -o -4 route show to default | ${awk} '{print $5}')
-          '';
-          peers = [
-            {
-              publicKey = "bUVmd8sOGyhFD6MKe1r3Why2WscKxNdUhBUDBVA9zUo=";
-              allowedIPs = [
-                "192.168.48.0/20"
-                "10.20.0.0/16"
-                "65.132.32.128/25"
-                "2001:428:6002:400::/56"
-              ];
-              endpoint = "65.132.32.180:31194";
-            }
-          ];
-        };
-        collo =
-          let awk = "${pkgs.gawk}/bin/awk";
-              endpoint = "64.16.52.138";
-          in {
-          privateKeyFile = "/root/vpn/wg_collo.private.key";
-          address = [ "10.10.10.87" ];
-          dns = ["10.10.10.1" "galois.com"];
-          postUp = ''
-            ip route add ${endpoint} via $(ip -o -4 route show to default | ${awk} '{print $3}') dev $(ip -o -4 route show to default | ${awk} '{print $5}')
-          '';
-          postDown = ''
-            ip route del ${endpoint} via $(ip -o -4 route show to default | ${awk} '{print $3}') dev $(ip -o -4 route show to default | ${awk} '{print $5}')
-          '';
-          peers = [
-            {
-              publicKey = "9xqBm/8JNWWyfB2CG3LtETxO4LZJqvnu9HidfJXSukk=";
-              allowedIPs = [
-                "10.10.0.0/16"
-                "64.16.52.128/25"
-              ];
-              endpoint = "${endpoint}:31194";
-            }
-          ];
-        };
-
         galois =
           let awk = "${pkgs.gawk}/bin/awk";
           in {
