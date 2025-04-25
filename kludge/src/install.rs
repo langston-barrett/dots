4d5b622fcfd210e54d3985af4a1c9e40abfd4bbc
Langston Barrett <langston.barrett@gmail.com>
2025-04-25 10:02:19 -0400

    .

--- kludge/src/install.rs
+++ kludge/src/install.rs
@@ -5,6 +5,8 @@ pub(super) fn go() -> Result<(), Box<dyn Error>> {
     let config_home = PathBuf::from(env::var("XDG_CONFIG_HOME")?);
     let here = env::current_dir()?;
     for (src, dst) in [
+        ("tmux", config_home.join("tmux")),
+        ("tmux/tmux.conf", home.join(".tmux.conf")),
         ("qutebrowser", config_home.join("qutebrowser")),
         ("qutebrowser", home.join(".qutebrowser")),
     ] {
