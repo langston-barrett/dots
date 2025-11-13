cdf239bdf854f2d3661c4055f7dec5d7e4832090
Langston Barrett <langston.barrett@gmail.com>
2025-11-13 03:56:28 +0000

    .

--- kludge/src/launcher.rs
+++ kludge/src/launcher.rs
@@ -10,6 +10,7 @@ use anyhow::Context as _;
 const DELIM: &str = "\t";
 
 fn apps(stdin: &mut fs::File) -> anyhow::Result<()> {
+    #[cfg(target_os = "macos")]
     if let Ok(it) = fs::read_dir("/Applications") {
         for entry in it {
             let entry = entry?;
@@ -28,19 +29,50 @@ fn apps(stdin: &mut fs::File) -> anyhow::Result<()> {
             stdin.write_all(b"\n")?;
         }
     }
+
+    #[cfg(target_os = "linux")]
+    {
+        let path_var = env::var("PATH").expect("PATH not found");
+        let mut paths = path_var.split(':').collect::<Vec<_>>();
+        paths.dedup();
+        let mut bins = Vec::with_capacity(64); // guess
+        for path in paths {
+            let dir = std::path::PathBuf::from(path);
+            if let Ok(entries) = fs::read_dir(&dir) {
+                for entry in entries {
+                    let entry = entry?;
+                    let ft = entry.file_type()?;
+                    if ft.is_file() || ft.is_symlink() {
+                        bins.push(entry.path());
+                    }
+                }
+            }
+        }
+        bins.dedup_by_key(|p| p.file_name().unwrap().to_owned());
+        for path in bins {
+            stdin.write_all(path.file_name().unwrap().as_bytes())?;
+            stdin.write_all(DELIM.as_bytes())?;
+            stdin.write_all(path.as_os_str().as_bytes())?;
+            stdin.write_all(b"\n")?;
+        }
+    }
     Ok(())
 }
 
 pub(super) fn go() -> anyhow::Result<()> {
     unsafe { env::remove_var("ITERM_PROFILE") };
 
+    #[cfg(target_os = "macos")]
     let p = "/Users/langston/.launcher";
+    #[cfg(target_os = "linux")]
+    let p = "/home/langston/.launcher";
+
     if let Ok(stdin) = fs::File::open(p) {
         Err(process::Command::new("pick")
             .stdin(stdin)
             .arg(format!("--delimiter={DELIM}"))
             .arg("--with-nth={1}")
-            .arg("--bind=enter:become(zsh -ic {2})")
+            .arg("--bind=enter:become({2})")
             .arg("--preview-window=hidden")
             .exec())?;
     }
@@ -50,7 +82,10 @@ pub(super) fn go() -> anyhow::Result<()> {
 
     stdin.write_all("Tasks".as_bytes())?;
     stdin.write_all(DELIM.as_bytes())?;
+    #[cfg(target_os = "macos")]
     stdin.write_all("/Users/langston/code/dots/files/scripts/bin/tasks".as_bytes())?;
+    #[cfg(target_os = "linux")]
+    stdin.write_all("tasks".as_bytes())?;
 
     stdin.flush()?;
     stdin.rewind()?;
@@ -59,7 +94,7 @@ pub(super) fn go() -> anyhow::Result<()> {
         .stdin(stdin)
         .arg(format!("--delimiter={DELIM}"))
         .arg("--with-nth={1}")
-        .arg("--bind=enter:become(zsh -ic {2})")
+        .arg("--bind=enter:become({2})")
         .arg("--preview-window=hidden")
         .exec())?
 }
