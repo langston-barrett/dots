e0fc1b486cb1f2f0ff5c59f557415d767318d499
Langston Barrett <langston.barrett@gmail.com>
2025-11-13 03:21:14 +0000

    .

--- kludge/src/install.rs
+++ kludge/src/install.rs
@@ -1,5 +1,5 @@
 use anyhow::Context;
-use std::{env, error::Error, os::unix::fs::symlink, path::PathBuf};
+use std::{env, error::Error, fs, os::unix::fs::symlink, path::PathBuf};
 
 pub(super) fn go() -> Result<(), Box<dyn Error>> {
     let home = PathBuf::from(env::var("HOME")?);
@@ -7,14 +7,18 @@ pub(super) fn go() -> Result<(), Box<dyn Error>> {
         PathBuf::from(env::var("XDG_CONFIG_HOME").context("XDG_CONFIG_HOME not set")?);
     let here = env::current_dir()?;
     for (src, dst) in [
-        ("tmux", config_home.join("tmux")),
-        ("tmux/tmux.conf", home.join(".tmux.conf")),
+        ("inputrc", home.join(".inputrc")),
+        ("polybar.ini", config_home.join("polybar/config.ini")),
         ("qutebrowser", config_home.join("qutebrowser")),
         ("qutebrowser", home.join(".qutebrowser")),
-        ("zshrc", home.join(".zshrc")),
-        ("zsh.d", home.join(".zsh.d")),
         ("sh.d", home.join(".sh.d")),
+        ("taskrc", config_home.join("task/taskrc")),
+        ("tmux", config_home.join("tmux")),
+        ("tmux/tmux.conf", home.join(".tmux.conf")),
+        ("zsh.d", home.join(".zsh.d")),
+        ("zshrc", home.join(".zshrc")),
     ] {
+        fs::create_dir_all(dst.parent().unwrap())?;
         if dst.is_symlink() {
             continue;
             // fs::remove_file(&dst)?;
